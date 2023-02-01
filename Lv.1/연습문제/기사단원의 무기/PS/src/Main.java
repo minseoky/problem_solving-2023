6e8c5030cf5d6876079a2c3a43f022417671b719
minseoky <minseok184@gmail.com>
2023-02-01 18:42:48 +0900

    add:lv1 기사단원의 무기

---
+++ Lv.1/연습문제/기사단원의 무기/PS/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int number = 10;
+        int limit = 3;
+        int power = 2;
+
+        System.out.println(sol.solution(number, limit, power));
+    }
+}
---
+++ Lv.1/연습문제/기사단원의 무기/PS/src/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int requiredIron = 0;
+        for(int i = 1 ; i <= number ; i++){
+            int expectedIron = sumDivisorCount(i); // 예상 요구량
+            if(expectedIron <= limit){ // 초과하지 않았을경우
+                requiredIron += expectedIron;
+            }
+            else{ //초과했을 경우
+                requiredIron += power;
+            }
+        }
+        return requiredIron;
+    }
+    public static int sumDivisorCount(int number){ // 인수로 받은 정수의 약수의 개수를 반환하는 함수
+        int answer = 0;
+        for(int i = 1 ; i*i <= number ; i++){
+            if(i*i == number){
+                answer += 1;
+            }
+            else if(number % i == 0){
+                answer += 2;
+            }
+        }
+        return answer;
+    }
+}
