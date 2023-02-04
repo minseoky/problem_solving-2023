e7904df24b0262f08a66c6601bafd8276e7acb2a
minseoky <minseok184@gmail.com>
2023-02-04 17:54:30 +0900

    lv.1 없는숫자더하기

---
+++ Lv.1/월간 코드 챌린지 시즌3/없는 숫자 더하기/PS/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] numbers = {1,2,3,4,6,7,8,0};
+        System.out.println(sol.solution(numbers));
+    }
+}
