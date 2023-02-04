cb953d7b0a92d6995082b7cf7cc0644e9f7ee656
minseoky <minseok184@gmail.com>
2023-02-04 17:17:22 +0900

    lv.1 나머지가 1이 되는 수 찾기

---
+++ Lv.1/월간 코드 챌린지 시즌3/나머지가 1이 되는 수 찾기/PS/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n = 12;
+        System.out.println(sol.solution(n));
+
+    }
+}
