a244ae910451895a85f276ecf0f632259fb1602b
minseoky <minseok184@gmail.com>
2023-02-06 22:10:57 +0900

    lv.1 약수의 개수와 덧셈

---
+++ Lv.1/월간 코드 챌린지 시즌2/약수의 개수와 덧셈/PS/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int left = 13;
+        int right = 17;
+        System.out.println(sol.solution(left, right));
+    }
+}
