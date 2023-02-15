3671ac99031594ec94f00ae194b5e6f0fb0cb76f
minseoky <minseok184@gmail.com>
2023-02-15 13:19:24 +0900

    add:lv.1내적

---
+++ Lv.1/월간 코드 챌린지 시즌1/내적/PS/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+
+        System.out.println(sol.solution(a,b));
+    }
+}
